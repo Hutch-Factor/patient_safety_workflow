54f0a0295b3ee15f28351db4b1e5dd3f82ec4483
Ray Hutchinson <85248738+Hutch-Factor@users.noreply.github.com>
2025-07-30 09:31:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,3 @@ import streamlit as st
 st.set_page_config(page_title="Patient Safety App", layout="wide")
 st.title("🛡️ Patient Safety Workflow Automation")
 st.markdown("Use the sidebar to navidgate to **Incident Tracker** or **Dashboard**.")
-
-
-
